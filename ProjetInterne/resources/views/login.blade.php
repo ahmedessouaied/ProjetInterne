27e1f69976da880e4a4f171017cc4da40262a340
20YoussefDhouib02 <youssefdhouib2014@gmail.com>
2024-03-26 17:56:28 +0100

    laravel fix

--- ProjetInterne/resources/views/login.blade.php
+++ ProjetInterne/resources/views/login.blade.php
@@ -3,7 +3,8 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="../css/login.css">
+  <link rel="stylesheet" href="{{ asset('css/login.css') }}">
+
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-..." crossorigin="anonymous" />
   <title>Document</title>
 </head>
@@ -129,6 +130,7 @@
 
 
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="../js/login.js"></script>
+<script src="{{ asset('js/login.js') }}"></script>
+
 </body>
 </html>
