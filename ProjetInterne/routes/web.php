5989619b60a0b6698b9647f798e6a886c9eed77e
Amal Jaoua <amal.jaoua@supcom.tn>
2024-03-27 17:33:54 +0100

    Single Article Page HTML CSS JS

--- ProjetInterne/routes/web.php
+++ ProjetInterne/routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/singlearticle', function () {
+    return view('singlearticle');
 });
